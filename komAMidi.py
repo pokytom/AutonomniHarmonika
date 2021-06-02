40e559584abdba8dd55506e4dc58e782ff630175
pokytom <38667968+pokytom@users.noreply.github.com>
2021-06-02 11:02:53 +0200

    Update komAMidi.py

--- komAMidi.py
+++ komAMidi.py
@@ -4,15 +4,14 @@ from os import path
 
 
 def play_song(file):
-	#ser = serial.Serial('/dev/ttyUSB0',9600)
-	#print(ser.name, ser.baudrate)
+	ser = serial.Serial('/dev/ttyUSB0',9600)
+	print(ser.name, ser.baudrate)
 	file_path = '{}{}'.format('./songs/', file)
 	if file[-3:] != 'mid' or not path.exists(file_path):
 		return False
 	for msg in MidiFile(file_path).play():
 		val = msg.dict()
-		#output = ser.write((tuple(val.items())[3][1]))
-		print(tuple(val.items())[3][1])
-		#f.write(output)
-	#ser.close()
+		output = ser.write((tuple(val.items())[3][1]))
+		#print(tuple(val.items())[3][1])
+	ser.close()
 	return True
